fd7dd2a082fd4ee826005de857ee31a2e4c344e1
Weikko Aejmelaeus <weikko.aejmelaeus@gmail.com>
2016-03-28 17:56:02 +0200

    added tests project

---
+++ NServiceBus.SequenceGate.Tests/Tests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace NServiceBus.SequenceGate.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.Pass("Not much going on here...");
+        }
+    }
+}
