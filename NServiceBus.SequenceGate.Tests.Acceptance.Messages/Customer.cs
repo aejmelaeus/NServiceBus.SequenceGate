c1a46cbf719c4d48b5694573c20734430d375941
Weikko Aejmelaeus <weikko.aejmelaeus@gmail.com>
2016-06-26 14:07:30 +0200

    Deleted unused files and folders

--- NServiceBus.SequenceGate.Tests.Acceptance.Messages/Customer.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NServiceBus.SequenceGate.Tests.Acceptance.Messages
-{
-    public class Customer
-    {
-        public string Id { get; set; }
-        public string Name { get; set; }
-    }
-}
